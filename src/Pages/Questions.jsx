75d65891dedd4085306d8a2f23c491172e47795e
Aromal0708 <aromalsunil12@gmail.com>
2023-10-23 20:09:33 +0530

    Design ready.Only api integration left

--- src/Pages/Questions.jsx
+++ src/Pages/Questions.jsx
@@ -1,16 +1,29 @@
-import { Box, Typography } from "@mui/material"
+import { Box, Button, Grid, Typography } from "@mui/material";
 
 const Questions = () => {
   return (
     <Box mt={4}>
-      <Typography variant="h3">
-        Question 1
-      </Typography>
-      <Typography variant="h5" mt={5} >
+      <Typography variant="h3">Question 1</Typography>
+      <Typography variant="h5" mt={5}>
         This is the question ?
       </Typography>
-    </Box>
-  )
-}
 
-export default Questions
+      <Grid my={3} container columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid my={1} item xs={6}>
+          <Button fullWidth variant="contained">Option 1</Button>
+        </Grid>
+        <Grid my={1} item xs={6}>
+          <Button fullWidth variant="contained">Option 2</Button>
+        </Grid>
+        <Grid my={1} item xs={6}>
+          <Button fullWidth variant="contained">Option 3</Button>
+        </Grid>
+        <Grid my={1} item xs={6}>
+          <Button fullWidth variant="contained">Option 4</Button>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default Questions;
--- src/Pages/Score.jsx
+++ src/Pages/Score.jsx
@@ -1,8 +1,19 @@
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import { Box, Button, Container, Typography } from "@mui/material";
 
 const Score = () => {
   return (
-    <div>Score</div>
-  )
-}
+    <>
+      <Box mt={20}>
+        <Typography variant="h2">Your Score : 10/10</Typography>
+        <Box mt={2}>
+          <Button startIcon={<ArrowForwardIcon/>} variant="outlined">
+            Return to Home Screen
+          </Button>
+        </Box>
+      </Box>
+    </>
+  );
+};
 
-export default Score
+export default Score;
